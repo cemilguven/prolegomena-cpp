43cb22f174eb51ef0d816b8311734a41070137dd
James Alden <james.alden314159@gmail.com>
2026-02-04 21:15:55 +0300

    Update and rename ee_calculator.cpp to multi_purpose_calculator.cpp
    Moved all circuit analysis calculations into a dedicated function to keep main() simple.
    
    Modified the history feature to allow results to be saved across different calculation menus.
    
    Created and utilized more functions.
    
    Created a custom goback() function to handle post-calculation results and removed the repetitive 'Would you like to continue?' prompts. Instead of answering a question every time, now pressing on [Enter] will be enough to continue.
    
    Implemented unique loops to allow users to perform multiple calculations without being kicked to the main menu.
    
    Made some changes to the logic to prevent logic leaks.
    
    Added a Matrix Calculator menu. (Work in progress)
    
    Renamed to multi_purpose_calculator.cpp due to its increasing features and possible future updates.
    
    Refactored.

--- multi_purpose_calculator.cpp
+++ multi_purpose_calculator.cpp
@@ -31,44 +31,102 @@ void clearscreen() {
 	std::cout << "\033[2J\033[H" << std::flush;
 }
 
+void goback() {
+	std::cout << "\nPress [Enter] to go back to menu...";
+	std::cin.ignore();
+	std::cin.get();
+	clearscreen();
+}
+
+std::vector<double> history;
+std::string userInput;
+
+void circuit_analysis_menu();
+
 int main() {
 
-	std::vector<double> history;
+	bool main_menu = true;
 
-	bool keepRunning = true;
-	std::string userInput;
-	do {
+	while (main_menu) {
 
-		int choice;
-		int choice2;
-		int choice3;
-
-		double r;
-		double i;
-		double v;
-		double p;
-
-		std::cout << "1. Ohm's Law Calculator\n";
-		std::cout << "\n2. Power Calculator\n";
-		std::cout << "\n3. View History\n";
+		std::cout << "--------------------------------\n";
+		std::cout << "----MULTI PURPOSE CALCULATOR----\n";
+		std::cout << "--------------------------------\n";
+		std::cout << "\n1. Circuit Analysis Calculator\n";
+		std::cout << "\n2. Matrix Calculator\n";
+		std::cout << "\n3. History\n";
 		std::cout << "\n4. Exit\n";
 		std::cout << "\n";
-		std::cin >> choice;
-
-
+		int choice0;
+		std::cin >> choice0;
 		clearscreen();
 
-		if (choice == 1) {
+			if (choice0 == 1) {
+				circuit_analysis_menu();
+			}
+
+			else if (choice0 == 3) {
+				std::cout << "--- Calculation History ---" << '\n';
+				if (history.empty()) {
+					clearscreen();
+					std::cout << "No history yet.\n";
+					goback();
+
+				}
+				else {
+					for (double n : history) {
+						std::cout << n << '\n';
+						goback();
+					}
+				}
+
+			}
+
+			else if (choice0 == 4) {
+				main_menu = false;
+				break;
+			}
+
+			else {
+				clearscreen();
+				std::cout << "Invalid Input.\n";
+				std::cout << "\n";
+			}
+		}
+	}
+
+void circuit_analysis_menu() {
+
+	bool in_circuit_mode = true;
+	int sub_choice1;
+	int sub_choice2;
+	int sub_choice3;
+	int sub_choice0;
+
+	while (in_circuit_mode) {
+
+		std::cout << "-------------------------------\n";
+		std::cout << "-----CIRCUIT ANALYSIS MENU-----\n";
+		std::cout << "-------------------------------\n";
+		std::cout << "\n1. Ohm's Law Calculator\n";
+		std::cout << "\n2. Power Calculator\n";
+		std::cout << "\n3. Go Back\n";
+		std::cout << "\n";
+		std::cin >> sub_choice0;
+		clearscreen();
+
+		if (sub_choice0 == 1) {
 
 			std::cout << "1. Voltage Calculator [I * R]" << '\n';
 			std::cout << "\n2. Current Calculator [V / R]" << '\n';
 			std::cout << "\n3. Resistance Calculator [V / I]" << '\n';
 			std::cout << "\n";
-			std::cin >> choice2;
+			std::cin >> sub_choice1;
 
 
 			clearscreen();
-			if (choice2 == 1) {
+			if (sub_choice1 == 1) {
+				double i, r;
 
 				std::cout << "Enter Current (A): ";
 				std::cin >> i;
@@ -81,9 +139,11 @@ int main() {
 				double result = calculate_voltage(i, r);
 				std::cout << "Voltage: " << result << " V" << '\n';
 				history.push_back(result);
+				goback();
 			}
 
-			else if (choice2 == 2) {
+			else if (sub_choice1 == 2) {
+				double v, r;
 
 				std::cout << "Enter Voltage (V): ";
 				std::cin >> v;
@@ -96,9 +156,11 @@ int main() {
 				double result = calculate_current(v, r);
 				std::cout << "Current: " << result << " A" << '\n';
 				history.push_back(result);
+				goback();
 			}
 
-			else if (choice2 == 3) {
+			else if (sub_choice1 == 3) {
+				double v, i;
 
 				std::cout << "Enter Voltage (V): ";
 				std::cin >> v;
@@ -111,25 +173,24 @@ int main() {
 				double result = calculate_resistance(v, i);
 				std::cout << "Resistance: " << result << " ohms" << '\n';
 				history.push_back(result);
+				goback();
 
-			}
-
-			else {
-				clearscreen();
-				std::cout << "Invalid Input.\n";
 			}
 
 		}
 
-		else if (choice == 2) {
+		else if (sub_choice0 == 2) {
 
 			std::cout << "1. Using Voltage and Resistance [V^2 / R] " << '\n';
 			std::cout << "\n2. Using Current and Resistance [I^2 * R] " << '\n';
 			std::cout << "\n3. Using Voltage and Current [V * I]" << '\n';
 			std::cout << "\n";
-			std::cin >> choice3;
+			std::cin >> sub_choice3;
 
-			if (choice3 == 1) {
+
+
+			if (sub_choice3 == 1) {
+				double v, r;
 
 				clearscreen();
 
@@ -144,9 +205,11 @@ int main() {
 				double result = calculate_power_from_voltage_resistance(v, r);
 				std::cout << "Power: " << result << " W" << '\n';
 				history.push_back(result);
+				goback();
 			}
 
-			else if (choice3 == 2) {
+			else if (sub_choice3 == 2) {
+				double i, r;
 
 				clearscreen();
 
@@ -161,9 +224,11 @@ int main() {
 				double result = calculate_power_from_current_resistance(i, r);
 				std::cout << "Power: " << result << " W" << '\n';
 				history.push_back(result);
+				goback();
 
 			}
-			else if (choice3 == 3) {
+			else if (sub_choice3 == 3) {
+				double v, i;
 
 				clearscreen();
 
@@ -178,52 +243,23 @@ int main() {
 				double result = calculate_power(v, i);
 				std::cout << "Power: " << result << " W" << '\n';
 				history.push_back(result);
+				goback();
+			}
+		}
+
+			else if (sub_choice0 == 3) {
+
+				in_circuit_mode = false;
+				clearscreen();
 			}
 
 			else {
 				clearscreen();
 				std::cout << "Invalid Input.\n";
+				std::cout << "\n";
+				goback();
 			}
 
-		}
-		else if (choice == 3) {
-			std::cout << "--- Calculation History ---" << '\n';
-			if (history.empty()) {
-				clearscreen();
-				std::cout << "No history yet :3\n";
+	}
 
-			}
-			else {
-				for (double n : history) {
-					std::cout << n << '\n';
-				}
-			}
-
-		}
-		else if (choice == 4) {
-			keepRunning = false;
-			std::cout << "Bye!" << '\n';
-			break;
-		}
-
-		else {
-			clearscreen();
-			std::cout << "Invalid Input.\n";
-		}
-
-		std::cout << "\nWould you like to continue? (Yes or No): ";
-		std::cin >> userInput;
-		clearscreen();
-
-		if (userInput == "yes" || userInput == "y" || userInput == "Yes" || userInput == "YES") {
-			keepRunning = true;
-			clearscreen();
-		}
-		else if (userInput == "no" || userInput == "n" || userInput == "No" || userInput == "NO") {
-			keepRunning = false;
-			std::cout << "Bye!\n";
-		}
-
-	} while (keepRunning);
-	return 0;
-}
+	}
