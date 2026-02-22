3582c64b9ec3535ad8eb7e82c62bc404bfa86003
James Alden <james.alden314159@gmail.com>
2026-02-22 19:50:59 +0300

    Update argus_core.cpp

--- argus_core.cpp
+++ argus_core.cpp
@@ -4,58 +4,58 @@ const int center_pwm = 1520;
 const int pwm_min = 520;
 const int pwm_max = 2520;
 const int pwm_total = pwm_max - pwm_min;
+
 const int camera_width = 640; //// !!!!!DONT FORGET BENCHMARK TEST 320 AND 1280!!!!!
-const int target_tolerance = 20; // +-
+const int target_x_tolerance = 20; // +- %3.125
+
+const int camera_length = 400;
+const int target_y_tolerance = 20; // +- %5
 
 struct Target {
     int x, y;
     int distance;
     double timestamp;
+    bool y_islocked;
+    bool x_islocked;
     bool islocked;
     int pwm;
+    int pwm_x;
+    int pwm_y;
 };
 
 void updateTarget(Target& t) {
 
     if (t.x > camera_width) t.x = camera_width;  // !!!!!DONT FORGET BENCHMARK TEST 320 AND 1280!!!!!
     if (t.x < 0) t.x = 0;
-    t.pwm = pwm_min + (t.x * pwm_total / camera_width);
+    t.pwm_x = pwm_min + (t.x * pwm_total / camera_width);
 
-    if (t.x > 300 && t.x < 340) { 
-        t.islocked = true;
-    }
+    if (t.y > camera_length) t.y = camera_length;
+    if (t.y < 0) t.y = 0;
+    t.pwm_y = pwm_min + (t.y * pwm_total / camera_length);
 
-    else {
-        t.islocked = false;
-    }
+    if (t.x > 300 && t.x < 340) t.x_islocked = true;
+    else { t.x_islocked = false; }
+
+    if (t.y > 180 && t.y < 220) t.y_islocked = true;
+    else t.y_islocked = false; 
+
+    t.islocked = (t.y_islocked && t.x_islocked);
 }
 
 int main()
 {
-    std::cout << "Test" << '\n';
-    Target Target1{ 100, 150, 25, 25.50, false, 1520 };
+    Target Target1{ 100, 100, 25, 25.50, false, false, false, 520, 0, 0 };
 
-    while (Target1.x < camera_width)
+    while (Target1.x < camera_width && Target1.y < camera_length)
     {
         updateTarget(Target1);
-        std::cout << '\n' << "X: " << Target1.x << '\n' << "PWM: " << Target1.pwm << " us" << '\n' << "--------";
+        std::cout << '\n' << "X: " << Target1.x << '\n' << "Y: " << Target1.y << '\n' << "PWM_X:" << Target1.pwm_x << " us" << '\n' << "PWM Y:" << Target1.pwm_y << " us" << '\n' << "--------";
         if (Target1.islocked) std::cout << " [ LOCKED ] --------";
-        Target1.x = Target1.x + 10;
+        Target1.x = Target1.x + 20;
+        Target1.y = Target1.y + 10;
     }
+
     std::cout << '\n';
     std::cout << '\n' << "--- COMPLETE ---" << '\n';
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-/* void updateTarget(Target* t) { t->x = t->x + 10; std::cout << "Coordinates:" << '\n'; std::cout << t->x << '\n' << t->y; 
-Target Target1;Target1.x = 100;Target1.y = 150;Target1.distance = 25;Target1.timestamp = 12.25;Target1.islocked = true;*/
