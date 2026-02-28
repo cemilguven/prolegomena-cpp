1cfc81008826a7b89042e5adabf53c3b4b18fbcd
James Alden <james.alden314159@gmail.com>
2026-02-28 13:49:58 +0300

    Update argus_vision.cpp

--- argus_vision.cpp
+++ argus_vision.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <chrono>
+#include <vector>
 
 constexpr int crosshair_gap = 2;
 constexpr int crosshair_length = 6;
@@ -10,7 +11,7 @@ int main() {
 
     cv::VideoCapture cap(0);
     cv::Mat webcam_main;
-    cv::Mat webcam_hsv,webcam_gray, webcam_binary_mask;
+    cv::Mat webcam_hsv, webcam_gray, webcam_binary_mask;
 
     int min_hue = 0, min_saturation = 0, min_value = 0;
     int max_hue = 179, max_saturation = 255, max_value = 255;
@@ -27,19 +28,16 @@ int main() {
 
     if (!cap.isOpened()) {
         std::cout << "[ERROR] CAMERA NOT DETECTED!" << '\n';
-
         return -1;
-
     }
 
     std::ofstream data_log("argus_telemetry.csv");
-    data_log << "Elapsed Time,X,Y,Area" << '\n';
+    data_log << "Elapsed Time,X,Y,Area" << '\n'; // , to separate into columns
     auto start_time = std::chrono::high_resolution_clock::now();
 
     while (true) {
 
         cap >> webcam_main;
-
         if (webcam_main.empty())
             break;
 
@@ -47,11 +45,25 @@ int main() {
 
         cv::Scalar lower_orange(min_hue, min_saturation, min_value);
         cv::Scalar upper_orange(max_hue, max_saturation, max_value);
-
         cv::inRange(webcam_hsv, lower_orange, upper_orange, webcam_binary_mask);
-        cv::Moments m = cv::moments(webcam_binary_mask, true);
 
-        if (m.m00 > 150) { 
+        std::vector<std::vector<cv::Point>> contours;
+        cv::findContours(webcam_binary_mask, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+        double max_area = 0; // largest area found while running
+        int best_match = -1; // -1 not found - starts from 0 (found)
+
+        for (int i = 0; i < contours.size(); i++) {
+            double area = cv::contourArea(contours[i]); // calcualtes each one
+            if (area > max_area) { // only updates when finds smthn bigger
+                max_area = area;
+                best_match = i; // saves the biggest one
+            }
+        }
+
+        if (best_match != -1 && max_area > 150) { // max_area same logic as (m.m00 > 150)  ALSO! could this be used to calculate z? distance? Z ! Z ! Z ! Z ! Z ! Z !  
+            //the smaller the pixel farther away it is and the bigger it is, the closer is?? !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            cv::Moments m = cv::moments(contours[best_match]);
 
             target_x = m.m10 / m.m00; // m00 = number of white pixels -- m10 = sum of x positions of white pixels -- m01 = sum of y positions of white pixels
             target_y = m.m01 / m.m00;
@@ -70,7 +82,7 @@ int main() {
             std::string hud_text = " X: " + std::to_string(static_cast<int>(target_x)) + " Y: " + std::to_string(static_cast<int>(target_y));
             cv::putText(webcam_main, hud_text, cv::Point(target_x + 15, target_y + 15), cv::FONT_HERSHEY_SIMPLEX, 0.35, cv::Scalar(0, 255, 0), 1, cv::LINE_8, false);
 
-            std::cout << "TARGET DETECTED AT -> X: " << static_cast<int>(target_x) << " | Y: " << static_cast<int>(target_y) << '\n'; // https://www.reddit.com/r/cpp_questions/comments/13i1282/stdendl_or_n/
+            std::cout << "TARGET DETECTED AT -> X: " << static_cast<int>(target_x) << " | Y: " << static_cast<int>(target_y) << '\n'; 
         }
 
         else {
